a8e426d38e591c5422efadc38c120165b50aebe6
Vincent Schall <v.schall@unibas.ch>
2025-09-21 21:47:40 +0200

    removed stuff

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,41 +1,8 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
 function cd... 		{ cd ..\.. }
 function cd.... 	{ cd ..\..\.. }
-function cd..     { cd .. }
-function n 		    { notepad $args }
 function inv      { Invoke-Item $args }
-function f        { Start-Process Firefox }
-function cl       { clear; ls }
-function admin
-{
-	if ($args.Count -gt 0)
-	{
-		$arglist = "& '" + $args + "'"
-		Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arglist
-	}
-	else 
-	{
-		Start-Process "$psHome\powershell.exe" -Verb runAs
-	}
-}
-Set-Alias -Name su -Value admin
-Set-Alias -Name sudo -Value admin
 function uni { cd $HOME\Documents\Uni\2025_HS }
-function gcom 
-{
-	git add .
-	git commit -m "$args"
-}
-function lazyg
-{
-	git add .
-	git commit -m "$args"
-	git push
-}
-function Get-PubIP 
-{
-	(Invoke-WebRequest http://ifconfig.me/ip ).Content
-}
 function find-file($name)
 {
 	ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
