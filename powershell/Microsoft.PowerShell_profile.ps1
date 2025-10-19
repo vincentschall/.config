114419747dcf8c33804a39b89bcb3d5173dd783a
Vincent Schall <v.schall@unibas.ch>
2025-10-19 12:32:14 +0200

    update powershell profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,4 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
-function cd... 		{ cd ..\.. }
-function cd.... 	{ cd ..\..\.. }
 function inv      { Invoke-Item $args }
 function uni { cd $HOME\Documents\Uni\2025_HS }
 function find-file($name)
@@ -12,27 +10,7 @@ function find-file($name)
 }
 function t
 {
-	git add . && git commit -m "task" && git push
-}
-function grep($regex, $dir)
-{
-	if ( $dir) 
-	{
-		ls $dir | select-string $regex
-		return
-	}
-	$input | select-string $regex
-}
-function touch($file) 
-{
-	"" | Out-File $file -Encoding ASCII
-}
-function pkill($name)
-{
-	ps $name -ErrorAction SilentlyContinue | kill
-}
-function pgrep($name)
-{
-	ps $name
+git add . && git commit -m "task" && git push
 }
 function usb { cd 'D:' }
+function movd { Move-Item ~/Downloads/* . }
