418e954c2f29e587a7ac171bbd117333fdb8ac68
Vincent Schall <v.schall@unibas.ch>
2025-10-27 23:11:46 +0100

    task

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,5 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
-function inv      { Invoke-Item $args }
 function uni { cd $HOME\Documents\Uni\2025_HS }
-function find-file($name)
-{
-	ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
-		$place_path = $_.directory
-		echo "${place_path}\${_}"
-	}
-}
 function t
 {
 git add . && git commit -m "task" && git push
